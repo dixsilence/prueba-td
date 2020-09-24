105918c31067f7fae642c3e8253ee7b4acc31c2b
rodrigo_calderon <dixsilence@gmail.com>
2020-09-24 19:45:05 -0300

    first commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,21 @@
+$('#carrusel-productos').carousel({
+    interval: 8000
+})
+
+$('#carrusel-productos .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
